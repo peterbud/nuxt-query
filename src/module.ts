142d8d4669ee87fc923a1b74bef914a7c1e319c1
Peter Budai <peterbudai@hotmail.com>
2025-02-14 15:28:18 +0100

    chore: initialize Nuxt Query module with basic setup and configurations

---
+++ src/module.ts
@@ -0,0 +1,20 @@
+import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+
+// Module options TypeScript interface definition
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface ModuleOptions {}
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: 'nuxt-query',
+    configKey: 'myModule',
+  },
+  // Default configuration options of the Nuxt module
+  defaults: {},
+  setup(_options, _nuxt) {
+    const resolver = createResolver(import.meta.url)
+
+    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
+    addPlugin(resolver.resolve('./runtime/plugin'))
+  },
+})
