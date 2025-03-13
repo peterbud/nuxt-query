bdfcbe3580028f2358d978b42fcdd3f3d6176e61
Peter Budai <peterbudai@hotmail.com>
2025-03-13 19:02:23 +0100

    feat: add minimal Nuxt example with nuxt-query

---
+++ examples/minimal/app.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+const getUsers = async () => {
+  return await $fetch('https://jsonplaceholder.typicode.com/users')
+}
+
+const { isPending: isPending, data: users } = useQuery({
+  queryKey: ['users'],
+  queryFn: getUsers,
+})
+</script>
+
+<template>
+  <h1>Users</h1>
+  <div v-if="isPending">
+    Loading...
+  </div>
+  <div
+    v-else
+    :style="{
+      display: 'grid',
+      gap: '0.5rem',
+      paddingLeft: '0.5rem',
+    }"
+  >
+    <div
+      v-for="user in users"
+      :key="user.id"
+      :style="{
+        cursor: 'pointer',
+        paddingTop: '0.25rem',
+        borderRadius: '0.125rem',
+      }"
+    >
+      {{ user.name }} ({{ user.email }})
+    </div>
+  </div>
+</template>
